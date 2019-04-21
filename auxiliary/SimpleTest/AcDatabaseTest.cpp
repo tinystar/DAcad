563308772daae085dac818b8b03182000b36dd31
wanghaixin <765392700@qq.com>
2019-04-21 22:33:39 +0800

    handletable测试代码维护

---
+++ auxiliary/SimpleTest/AcDatabaseTest.cpp
@@ -0,0 +1,34 @@
+#include "dbHandleTable.h"
+
+
+void testAcDbHandleTable()
+{
+	AcDbHandleTable* pHandleTable = new AcDbHandleTable(NULL, false);
+	AcDbStub* pStub = NULL;
+	Acad::ErrorStatus es = pHandleTable->addHandle(pStub, AcDbHandle(1));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(1));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(2));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(25));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(10));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(100));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(70));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(256));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(321));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(11));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(3));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(5));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(555));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(7));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(8));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(6));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(9));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(15));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(13));
+	es = pHandleTable->addHandle(pStub, AcDbHandle(14));
+	//delete pHandleTable;
+}
+
+void testDatabaseEntry()
+{
+	testAcDbHandleTable();
+}
--- auxiliary/SimpleTest/src/main.cpp
+++ auxiliary/SimpleTest/src/main.cpp
@@ -1,45 +1,10 @@
-#include "acFixedHeap.h"
-
-
-void testFixedSizeHeap()
-{
-// 	AcFixedSizeHeap* pHeap = __acHeapCreate(0x4000);
-// 
-// 	void* pMem1 = __acHeapMalloc(pHeap);
-// 	size_t size = __acHeapSize(pHeap);
-// 
-// 	pMem1 = __acHeapMalloc(pHeap);
-// 	pMem1 = __acHeapMalloc(pHeap);
-// 	pMem1 = __acHeapMalloc(pHeap);
-// 	pMem1 = __acHeapMalloc(pHeap);
-// 
-// 	__acHeapDestroy(pHeap);
-
-	void* pMem2 = __acMallocAligned(0x4000);
-	void* pMem3 = __acMallocAligned(20);
-
-	void* pMem4 = __acMallocAligned(0x4000);
-	void* pMem5 = __acMallocAligned(0x4000);
-	void* pMem6 = __acMallocAligned(0x4000);
-
-	__acFreeAligned(pMem4);
-	__acFreeAligned(pMem2);
-
-	void* pMem7 = __acMallocAligned(0x4000);
-	void* pMem8 = __acMallocAligned(0x4000);
-	void* pMem9 = __acMallocAligned(0x4000);
-
-	__acFreeAligned(pMem5);
-	__acFreeAligned(pMem7);
-	__acFreeAligned(pMem8);
-	__acFreeAligned(pMem6);
-
-	void* pMem10 = __acMallocAligned(0x4000);
-}
+extern void testFixedSizeHeap();
+extern void testDatabaseEntry();
 
 int main()
 {
-	testFixedSizeHeap();
+	//testFixedSizeHeap();
+	testDatabaseEntry();
 
 	return 0;
 }
